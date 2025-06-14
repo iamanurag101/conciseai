51a21c317e499aeb1e27ff842c3839c0f7fd360b
Anurag Dey <iamanuragdey005@gmail.com>
2025-06-14 20:58:15 +0530

    Create Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import HeroSection from "@/components/home/hero-section";
+import BgGradient from "@/components/common/bg-gradient";
 
 export default function Home() {
   return (
-    <div className="flex-1">
-      <Button>Hello</Button>
+    <div className="relative w-full">
+      <BgGradient/>
+      <div className="flex flex-col">
+        <HeroSection/>
+      </div>
+      {/* <DemoSection/> */}
+      {/* <HowItWorksSection/> */}
+
+      {/* <PricingSection/> */}
+
+      {/* <CTASection/> */}
     </div>
   );
 }
