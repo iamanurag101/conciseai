9d2f7c706b6421deda7de0f32a3cccfc06d1ed2a
Anurag Dey <iamanuragdey005@gmail.com>
2025-06-15 18:04:57 +0530

    Theme Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BgGradient from "@/components/common/bg-gradient";
 import HeroSection from "@/components/home/hero-section";
 import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
 
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection/>
         <DemoSection/>
+        <HowItWorksSection/>
       </div>
-      {/* <HowItWorksSection/> */}
 
       {/* <PricingSection/> */}
 
